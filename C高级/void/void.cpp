a00be455083e8c2630057f708978d7a41dff1afc
55082 <55082@ADMIN>
2024-02-23 10:36:25 +0800

    补充c高级的代码

---
+++ C高级/void/void.cpp
@@ -0,0 +1,13 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    void* p = NULL;
+    int* i = NULL;
+    char* c = NULL;
+    c = (char*)i;//不同类型需要强转
+    c = (char*)p;
+     
+    std::cout << "Hello World!\n";
+}
